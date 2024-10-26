0d5d66c8fda6a58dee8ccf9fe91b2ecf6c3ccd4d
Abdul Hameed <raza2022@gmail.com>
2024-10-26 17:49:38 +0500

    local pubic dist replaced with cdn dist

--- client/main.js
+++ client/main.js
@@ -1,5 +1,5 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 import "dwt";
+
+Dynamsoft.DWT.ResourcesPath = 'https://unpkg.com/dwt/dist/';
 
 import './main.html';
